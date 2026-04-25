1ba94460071b633d1091c8eca3d19405752decf5
ColinLug <colin.lugin@gmail.com>
2026-04-25 19:01:52 +0200

    fixed generator

--- chaos-aaaaaaaaaaaaaaaaaaaah/game_manager.gd
+++ chaos-aaaaaaaaaaaaaaaaaaaah/game_manager.gd
@@ -16,8 +16,9 @@ func generate_text():
 			var text_length = len(text_arr)
 			for i in range(round(text_length/3)):
 				var rand_word_index = randi_range(0, len(brainrot)-1)
+				var rand_pos = randi_range(0, text_length-1)
 				var appendix = "a" if rand_word_index-text_length%2==0 else "o"
-				text_arr[i] = brainrot[rand_word_index] + appendix
+				text_arr[rand_pos] = brainrot[rand_word_index] + appendix
 			var text_random = ""
 			for i in text_arr:
 				text_random += i + " "
