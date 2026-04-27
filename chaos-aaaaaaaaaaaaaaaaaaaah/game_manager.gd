6fc40249c80e8d6f0831981f0a90528e2d9d3660
ColinLug <colin.lugin@gmail.com>
2026-04-27 13:07:29 +0200

    brainrot, but not every words

--- chaos-aaaaaaaaaaaaaaaaaaaah/game_manager.gd
+++ chaos-aaaaaaaaaaaaaaaaaaaah/game_manager.gd
@@ -25,18 +25,18 @@ func _on_scene_changed():
 func generate_text():
 	for node in get_all_children(get_tree().current_scene):
 		if node is Label or node is RichTextLabel:
-			#var text_arr = node.text.split(" ")
-			#var text_length = len(text_arr)
-			#for i in range(round(text_length/3)):
-				#var rand_word_index = randi_range(0, len(brainrot)-1)
-				#var rand_pos = randi_range(0, text_length-1)
-				#var appendix = "a" if rand_word_index-text_length%2==0 else "o"
-				#text_arr[rand_pos] = brainrot[rand_word_index] + appendix
-			#var text_random = ""
-			#for i in text_arr:
-				#text_random += i + " "
-			#node.text = text_random.strip_edges()
-			node.text = _generator.generate_noun(_max_length, _temperature, _top_k)
+			var text_arr = node.text.split(" ")
+			var text_length = len(text_arr)
+			for i in range(round(text_length/3)):
+				var rand_word_index = randi_range(0, len(brainrot)-1)
+				var rand_pos = randi_range(0, text_length-1)
+				var appendix = "a" if rand_word_index-text_length%2==0 else "o"
+				text_arr[rand_pos] = _generator.generate_noun(_max_length, _temperature, _top_k) #brainrot[rand_word_index] + appendix
+			var text_random = ""
+			for i in text_arr:
+				text_random += i + " "
+			node.text = text_random.strip_edges()
+			#node.text = _generator.generate_noun(_max_length, _temperature, _top_k)
 			
 
 func get_all_children(in_node, array := []):
