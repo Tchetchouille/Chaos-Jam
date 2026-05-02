f7df77ee6dda98ef8395091b69656d383f0679d9
ColinLug <colin.lugin@gmail.com>
2026-05-02 18:14:25 +0200

    assassin de la police

--- chaos-aaaaaaaaaaaaaaaaaaaah/ennemies/assassin.gd
+++ chaos-aaaaaaaaaaaaaaaaaaaah/ennemies/assassin.gd
@@ -22,22 +22,19 @@ func _process(delta: float) -> void:
 		#$Label.visible = true
 		#$Timer.start()
 		#$AnimatedSprite2D.play("explode")
-	if abs(position.x - target.position.x)<500 && abs(position.x - target.position.x)>=100 and alive and abs(position.y - target.position.y)<100:
+	if abs(position.x - target.position.x)<500 && abs(position.x - target.position.x)>=70 and abs(position.y - target.position.y)<100:
 		print("pursuing")
 		var direction = 1 if position.x < target.position.x else -1
+		$AnimatedSprite2D.flip_h = true if direction<0 else false
 		velocity.x = pursuing_speed * direction
-		$AnimatedSprite2D.play("pursue")
-	elif alive :
-		print("Nope")
-		velocity.x = 0
+		#if not $AnimatedSprite2D.is_playing("sprint"):
+		$AnimatedSprite2D.play("sprint")
+		$Label.visible = false
 	
-	if abs(position.x - target.position.x)<100 and $Timer.is_stopped() and alive and abs(position.y - target.position.y)<100 :
-		alive = false
+	if abs(position.x - target.position.x)<70 and abs(position.y - target.position.y)<100:
 		velocity.x = 0
+		$AnimatedSprite2D.play("attack")
 		$Label.visible = true
-		$Timer.start()
-		$AnimatedSprite2D.play("explode")
-		
 	
 
 func _physics_process(delta: float) -> void:
@@ -45,15 +42,4 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 	move_and_slide()
 func die():
-	$Timer2.start()
-
-func _on_timer_timeout() -> void:
-	$Killzone.can_kill=true
-	for i in $Killzone.get_overlapping_bodies():
-		if i.has_method("die"):
-			i.die()
-	if $Timer2.is_stopped():
-		$Timer2.start()
-
-func _on_timer_2_timeout() -> void:
 	queue_free()
